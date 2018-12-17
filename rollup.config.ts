f0084e00e16dcd9bc180bcffe6db6a3fc6e70907
Joe Haddad <timer150@gmail.com>
2018-12-16 22:52:11 -0500

    build: emit CJS intead of UMD

--- rollup.config.ts
+++ rollup.config.ts
@@ -1,7 +1,6 @@
 import resolve from 'rollup-plugin-node-resolve'
 import commonjs from 'rollup-plugin-commonjs'
 import sourceMaps from 'rollup-plugin-sourcemaps'
-import camelCase from 'lodash.camelcase'
 import typescript from 'rollup-plugin-typescript2'
 import json from 'rollup-plugin-json'
 
@@ -10,15 +9,19 @@ const pkg = require('./package.json')
 export default {
   input: `src/${pkg.name}.ts`,
   output: [
-    {
-      file: pkg.main,
-      name: camelCase(pkg.name),
-      format: 'umd',
-      sourcemap: true
-    },
+    { file: pkg.main, format: 'cjs', sourcemap: true },
     { file: pkg.module, format: 'es', sourcemap: true }
   ],
-  external: [],
+  external: [
+    // built-ins
+    'fs',
+    'path',
+    'util',
+    'crypto',
+    // deps
+    'eol',
+    'debug'
+  ],
   watch: {
     include: 'src/**'
   },
